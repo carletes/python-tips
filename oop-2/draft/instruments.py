9afede1070f79d62c3a97bd5c551b9554c46649c
Carlos Valiente <carlos@pepelabs.net>
2012-11-27 20:09:37 +0000

    More modelling

--- oop-2/draft/instruments.py
+++ oop-2/draft/instruments.py
@@ -1,4 +1,27 @@
+"""Instrument definitiions.
+
+"""
+
+import os
+import urllib2
+
 import logging
+
+
+__all__ = [
+    "AMSU",
+    "ASCAT",
+    "AVHRR",
+    "HIRS",
+    "IASI",
+    "MHS",
+]
+
+
+
+def url_for(satellite, param):
+    return ("https://raw.github.com/carletes/python-tips/master/data/%s/%s" %
+            (satellite, param))
 
 
 class Instrument(object):
@@ -8,6 +31,8 @@ class Instrument(object):
     """
 
     description = None
+
+    params = None
 
     log = logging.getLogger("instruments")
 
@@ -20,7 +45,7 @@ class Instrument(object):
             (param, lat, lon, value)
 
         """
-        raise NotImplementedError("Override in derived class!")
+        raise NotImplementedError("Method not overriden!")
 
     def __str__(self):
         return "%s (%s)" % (self.name, self.description)
@@ -30,32 +55,67 @@ class ATOVS(Instrument):
 
     description = "Advanced TIROS Operational Vertical Sounder"
 
+    def measurements(self):
+        ret = []
+        for p in self.params:
+            self.log.debug("Retrieving %s data for %s", self.name, p)
+            url = url_for(self.name, p)
+            self.log.debug("%s: Fetching %s data from %s", self.name, p, url)
+            for line in urllib2.urlopen(url):
+                lat, lon, value = [float(v) for v in line.split()]
+                ret.append((p, lat, lon, value))
+
 
 class AMSU(ATOVS):
     
     description = "Advanced Microwave Sounding Unit"
+
+    params = ["temperature", "humidity"]
 
 
 class AVHRR(Instrument):
 
     description = "Advanced Very High Resolution Radiometer"
 
+    params = ["cloud_cover", "surface_temperature", "ice_cover", "snow_cover"]
+
 
 class HIRS(Instrument):
 
     description = "High Resolution Infra-red Sounder"
+
+    params = ["incident_radiation"]
+
+    def measurements(self):
+        ret = []
+        for p in self.params:
+            self.log.debug("Retrieving %s data for %s", self.name, p)
+            fname = os.path.join(os.path.dirname(__file__),
+                                 "data", self.name, p)
+            self.log.debug("%s: Fetching %s data from %s", self.name, p,
+                           fname)
+            with open(fname, "rt") as f:
+                for line in f:
+                    lat, lon, value = [float(v) for v in line.split()]
+                    ret.append((p, lat, lon, value))
 
 
 class IASI(Instrument):
 
     description = "Infrared Atmospheric Sounding Interferometer"
 
+    params = ["temperature", "humidity", "surface_temperature"]
+
 
 class MHS(Instrument):
 
     description = "Microwave Humidity Sounder"
 
+    params = ["temperature", "humidity"]
+
 
 class ASCAT(Instrument):
 
     description = "Advanced SCATterometer"
+
+    params = ["wind"]
